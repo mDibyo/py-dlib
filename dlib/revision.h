a61d109e85c17aad6f17c689d9e80f39a4b6dbd6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-18 15:59:03 -0700

    Initial commit with original library

---
+++ dlib/revision.h
@@ -0,0 +1,7 @@
+#ifndef DLIB_REVISION_H
+// Version:  18.8
+// Date:     Mon Jun 2 19:20:39 EDT 2014
+// Mercurial Revision ID:  a6c2b16111b8
+#define DLIB_MAJOR_VERSION  18
+#define DLIB_MINOR_VERSION  8
+#endif
